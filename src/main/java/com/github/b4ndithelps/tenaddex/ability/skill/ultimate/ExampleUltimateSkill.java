b268866bcd9b772d79ea3e705214ccce20e10404
Bandit <bandit.lbh@proton.me>
2024-12-02 12:10:23 -0500

    Added Example for a base Ultimate Skill (that does nothing)

---
+++ src/main/java/com/github/b4ndithelps/tenaddex/ability/skill/ultimate/ExampleUltimateSkill.java
@@ -0,0 +1,15 @@
+package com.github.b4ndithelps.tenaddex.ability.skill.ultimate;
+
+import com.github.manasmods.tensura.ability.skill.Skill;
+
+public class ExampleUltimateSkill extends Skill {
+
+    public ExampleUltimateSkill() {
+        // Pass in the type of the skill here.
+        super(SkillType.ULTIMATE);
+    }
+
+
+
+
+}
