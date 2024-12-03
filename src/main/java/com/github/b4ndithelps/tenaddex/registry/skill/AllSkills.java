72766533c3536230cc74bb6b3c5060c4e47b9e3a
Bandit <bandit.lbh@proton.me>
2024-12-02 19:17:46 -0500

    Intrinsic Example Skill Completed.

--- src/main/java/com/github/b4ndithelps/tenaddex/registry/skill/AllSkills.java
+++ src/main/java/com/github/b4ndithelps/tenaddex/registry/skill/AllSkills.java
@@ -4,6 +4,7 @@ package com.github.b4ndithelps.tenaddex.registry.skill;
 import com.github.b4ndithelps.tenaddex.TensuraAddonExample;
 import com.github.b4ndithelps.tenaddex.ability.skill.common.ExampleCommonSkill;
 import com.github.b4ndithelps.tenaddex.ability.skill.extra.ExampleExtraSkill;
+import com.github.b4ndithelps.tenaddex.ability.skill.intrinsic.ExampleIntrinsicSkill;
 import com.github.b4ndithelps.tenaddex.ability.skill.ultimate.ExampleUltimateSkill;
 import com.github.b4ndithelps.tenaddex.ability.skill.unique.ExampleUniqueSkill;
 import com.github.manasmods.manascore.api.skills.ManasSkill;
@@ -38,7 +39,8 @@ public class AllSkills {
     //   ====================
     //   | Intrinsic Skills |
     //   ====================
-
+    public static final RegistryObject<ExampleIntrinsicSkill> EXAMPLE_INTRINSIC =
+            skillRegistry.register("example_intrinsic", ExampleIntrinsicSkill::new);
     //   ================
     //   | Extra Skills |
     //   ================
