aeba6d7b4b28b9c2725c1ca8a174e56f341c4cde
Bandit <bandit.lbh@proton.me>
2025-01-21 17:55:57 -0500

    Race now shows up in the starting race selector. Needs to be added to the defaultconfigs though.

--- src/main/java/com/github/b4ndithelps/tenaddex/registry/race/AllRaces.java
+++ src/main/java/com/github/b4ndithelps/tenaddex/registry/race/AllRaces.java
@@ -3,6 +3,7 @@ package com.github.b4ndithelps.tenaddex.registry.race;
 import com.github.b4ndithelps.tenaddex.TensuraAddonExample;
 import com.github.b4ndithelps.tenaddex.race.ExampleRace;
 import com.github.manasmods.tensura.registry.race.TensuraRaces;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.IForgeRegistry;
@@ -10,6 +11,8 @@ import net.minecraftforge.registries.RegisterEvent;
 
 @Mod.EventBusSubscriber(modid = TensuraAddonExample.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class AllRaces {
+
+    public static final ResourceLocation EXAMPLE_RACE = new ResourceLocation(TensuraAddonExample.MODID, "example_race");
 
     @SubscribeEvent
     public static void register(RegisterEvent event) {
