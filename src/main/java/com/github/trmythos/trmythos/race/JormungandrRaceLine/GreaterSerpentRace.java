a04dafd9292b05870c64c162d77dab051b013741
electroreal1 <155461193+electroreal1@users.noreply.github.com>
2025-10-05 19:10:49 +0300

    a lot...
    Signed-off-by: electroreal1 <155461193+electroreal1@users.noreply.github.com>

--- src/main/java/com/github/trmythos/trmythos/race/JormungandrRaceLine/GreaterSerpentRace.java
+++ src/main/java/com/github/trmythos/trmythos/race/JormungandrRaceLine/GreaterSerpentRace.java
@@ -4,6 +4,7 @@ import com.github.manasmods.manascore.api.skills.ManasSkill;
 import com.github.manasmods.manascore.api.skills.SkillAPI;
 import com.github.manasmods.manascore.api.skills.capability.SkillStorage;
 import com.github.manasmods.tensura.ability.TensuraSkill;
+import com.github.manasmods.tensura.capability.race.TensuraPlayerCapability;
 import com.github.manasmods.tensura.race.Race;
 import com.github.manasmods.tensura.registry.race.TensuraRaces;
 import com.github.manasmods.tensura.registry.skill.CommonSkills;
