fe1c363e4a0709f7dd4ce0b0d4592dd7b49dd9b7
electroreal1 <155461193+electroreal1@users.noreply.github.com>
2025-10-06 16:43:59 +0300

    start of new and first unique skill. Omniscient Eye

---
+++ src/main/java/com/github/mythos/mythos/registry/MythosMobEffects.java
@@ -0,0 +1,25 @@
+package com.github.mythos.mythos.registry;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import java.awt.Color;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class MythosMobEffects {
+    private static final DeferredRegister<MobEffect> registry;
+
+    public MythosMobEffects() {
+    }
+
+    public static void registery(IEventBus modEventBus) {
+        registry.register(modEventBus);
+    }
+
+    static {
+        registry = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, "trmythos");
+    }
+}
+
