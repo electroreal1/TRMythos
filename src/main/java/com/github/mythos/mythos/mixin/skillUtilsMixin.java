756a6596c38420fd690426f7fbe56594b1b16f41
electroreal1 <155461193+electroreal1@users.noreply.github.com>
2025-10-20 16:22:21 +0300

    Profanity

--- src/main/java/com/github/mythos/mythos/mixin/skillUtilsMixin.java
+++ src/main/java/com/github/mythos/mythos/mixin/skillUtilsMixin.java
@@ -1,38 +1,49 @@
 package com.github.mythos.mythos.mixin;
 
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import com.github.manasmods.manascore.api.skills.ManasSkill;
 import com.github.manasmods.manascore.api.skills.ManasSkillInstance;
-import com.github.manasmods.tensura.ability.SkillUtils;
-import com.github.manasmods.tensura.capability.race.TensuraPlayerCapability;
-import com.github.manasmods.tensura.capability.skill.TensuraSkillCapability;
+import com.github.manasmods.tensura.config.TensuraConfig;
 import com.github.mythos.mythos.registry.skill.Skills;
-import net.minecraft.world.entity.player.Player;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+
+import static com.github.manasmods.tensura.ability.SkillUtils.isSkillToggled;
 
 public class skillUtilsMixin {
-    private static boolean hasSkill(Entity entity, ManasSkill skill) {
-        return false;
+    public static int getEarningLearnPoint(ManasSkillInstance instance, LivingEntity entity, boolean isMode) {
+        int point = TensuraConfig.INSTANCE.skillsConfig.bonusLearningGain.get() + entity.getRandom().nextInt(1, isMode ? 3 : 5);
+        if (isSkillToggled(entity, Skills.ORUNMILA.get())) {
+            point += 20;
+        }
+        return point;
     }
-    @ModifyReturnValue(method = "getEarningLearnPoint", at = @At("RETURN"), remap = false)
-    private static int modifyEarningLearnPoint(int basePoints, ManasSkillInstance instance, LivingEntity entity, boolean isMode) {
-        // Ultimate & Unique Skill Bonuses
-        if (hasSkill(entity, Skills.ELTNAM.get())) basePoints += 10;
 
-        return basePoints;
+    public static int getBonusMasteryPoint(ManasSkillInstance instance, LivingEntity entity, int originalPoint) {
+        int point = TensuraConfig.INSTANCE.skillsConfig.bonusMasteryGain.get();
+        if (isSkillToggled(entity, Skills.ORUNMILA.get())) {
+            point += 20 * originalPoint;
+        }
+        return point;
     }
-    @ModifyReturnValue(method = "getBonusMasteryPoint", at = @At("RETURN"), remap = false)
-    private static int modifyBonusMasteryPoint(int basePoints, ManasSkillInstance instance, LivingEntity entity) {
-        // Ultimate & Unique Skill Bonuses
-        if (hasSkill(entity, Skills.ELTNAM.get())) basePoints += 10;
-        return basePoints;
+
+    public static boolean canNegateDodge(LivingEntity entity, DamageSource source) {
+        Entity var3 = source.getEntity();
+        if (var3 instanceof LivingEntity living) {
+
+            if (isSkillToggled(entity, (ManasSkill)Skills.ORUNMILA.get())) {
+                return false;
+            }  else {
+                return living.getRandom().nextBoolean();
+            }
+        } else {
+            return false;
+        }
     }
+
+    public static boolean reducingResistances(LivingEntity living) {
+        return isSkillToggled(living, Skills.ORUNMILA.get());
+    }
+
+
 }
