927467d2280d153f22169ef69c1b3e55584e623c
electroreal1 <155461193+electroreal1@users.noreply.github.com>
2025-10-22 19:29:08 +0300

    .

--- src/main/java/com/github/mythos/mythos/util/MythosSkillUtils.java
+++ src/main/java/com/github/mythos/mythos/util/MythosSkillUtils.java
@@ -10,7 +10,7 @@ import net.minecraft.world.entity.LivingEntity;
 
 import static com.github.manasmods.tensura.ability.SkillUtils.isSkillToggled;
 
-public class SkillUtils extends com.github.manasmods.tensura.ability.SkillUtils {
+public class MythosSkillUtils extends com.github.manasmods.tensura.ability.SkillUtils {
     public static int getEarningLearnPoint(ManasSkillInstance instance, LivingEntity entity, boolean isMode) {
         int point = TensuraConfig.INSTANCE.skillsConfig.bonusLearningGain.get() + entity.getRandom().nextInt(1, isMode ? 3 : 5);
         if (isSkillToggled(entity, Skills.ORUNMILA.get())) {
