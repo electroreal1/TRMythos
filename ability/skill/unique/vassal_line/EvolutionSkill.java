b70965424a136198e8deaa6e520488194de5bf83
Jester <shirokodaki@gmail.com>
2025-10-30 02:58:02 +0200

    Delete ability/skill directory

--- ability/skill/unique/vassal_line/EvolutionSkill.java
+++
@@ -1,9 +0,0 @@
-package com.github.mythos.mythos.ability.skill.unique.vassal_line;
-
-import com.github.manasmods.tensura.ability.skill.Skill;
-
-public class EvolutionSkill extends Skill {
-    public EvolutionSkill(SkillType type) {
-        super(type);
-    }
-}
