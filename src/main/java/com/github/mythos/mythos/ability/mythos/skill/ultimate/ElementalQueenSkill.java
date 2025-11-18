aa79fc5af0cc83d6b1b10009a95048c9f29d90d8
Jester <shirokodaki@gmail.com>
2025-11-18 22:27:19 +0200

    The husk that is Elemental Queen

---
+++ src/main/java/com/github/mythos/mythos/ability/mythos/skill/ultimate/ElementalQueenSkill.java
@@ -0,0 +1,7 @@
+package com.github.mythos.mythos.ability.mythos.skill.ultimate;
+
+import com.github.manasmods.tensura.ability.skill.Skill;
+
+public class ElementalQueenSkill extends Skill {
+    public ElementalQueenSkill(SkillType type) {super(SkillType.ULTIMATE);}
+}
