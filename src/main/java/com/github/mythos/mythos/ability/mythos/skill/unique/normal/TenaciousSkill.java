48846e3dd410933df674b251722211cfb69dcbd2
Jester <shirokodaki@gmail.com>
2025-11-23 20:03:17 +0200

    Tenacious

--- src/main/java/com/github/mythos/mythos/ability/mythos/skill/unique/normal/TenaciousSkill.java
+++ src/main/java/com/github/mythos/mythos/ability/mythos/skill/unique/normal/TenaciousSkill.java
@@ -1,4 +1,4 @@
-package com.github.mythos.mythos.ability.mythos.skill.unique.normal;
+package com.github.mythos.mythos.ability.mythos.skill.unique;
 
 import com.github.manasmods.manascore.api.skills.ManasSkillInstance;
 import com.github.manasmods.tensura.ability.SkillHelper;
@@ -9,110 +9,119 @@ import com.github.manasmods.tensura.capability.race.TensuraPlayerCapability;
 import com.github.manasmods.tensura.client.particle.TensuraParticleHelper;
 import com.github.manasmods.tensura.registry.attribute.TensuraAttributeRegistry;
 import com.github.manasmods.tensura.util.damage.TensuraDamageSource;
+import com.github.mythos.mythos.registry.MythosMobEffects;
+import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Predicate;
 
 public class TenaciousSkill extends Skill {
-    public TenaciousSkill(SkillType type) {
-        super(SkillType.UNIQUE);
-    }
+    public TenaciousSkill(SkillType type) {super(SkillType.UNIQUE);}
 
-    @Override
+    //@Override
+    //public ResourceLocation getSkillIcon() {
+    //    return new ResourceLocation("trmythos", "textures/skill/unique/tenacious.png");
+    //}
+
+    public int getMaxMastery() {return 500;}
+
     public double getObtainingEpCost() {
         return 50000;
     }
 
-    @Override
+    public boolean canBeSlotted(ManasSkillInstance instance) {
+        return (instance.getMastery() < 0);
+    }
+
     public boolean canBeToggled(ManasSkillInstance instance, LivingEntity entity) {
         return true;
     }
 
-    @Override
-    public int getMaxMastery() {
-        return 4000;
+    public boolean canTick(ManasSkillInstance instance, LivingEntity entity) {
+        return instance.isToggled();
     }
 
-    @Override
-    public void onTick(ManasSkillInstance instance, @NotNull LivingEntity entity) {
-        if (!instance.isToggled()) return;
+    public void onTick(ManasSkillInstance instance, LivingEntity entity) {
+        if (instance.isToggled()) {
+            entity.addEffect(new MobEffectInstance((MobEffect) MythosMobEffects.ENERGIZED_REGENERATION.get(), 1200, 1, false, false, false));
+        }
+        if (entity instanceof Player) {
+            Player player = (Player)entity;
+            energizedHandler(player);
+        }
+        CompoundTag tag = instance.getOrCreateTag();
+        int time = tag.getInt("activatedTimes");
+        if (time % 12 == 0)
+            addMasteryPoint(instance, entity);
+        tag.putInt("activatedTimes", time + 1);
+    }
 
-        if (!(entity instanceof Player player)) return;
-
+    public void energizedHandler(Player player) {
         TensuraPlayerCapability.getFrom(player).ifPresent(cap -> {
             double maxMP = player.getAttributeValue(TensuraAttributeRegistry.MAX_MAGICULE.get());
+            double mpGain = maxMP * 0.001D;
+            if (cap.getMagicule() + mpGain < cap.getBaseMagicule()) {
+                cap.setMagicule(cap.getMagicule() + mpGain);
+            } else {
+                cap.setMagicule(cap.getBaseMagicule());
+            }
+
             double maxAP = player.getAttributeValue(TensuraAttributeRegistry.MAX_AURA.get());
-            double regenMPPerTick = (maxMP * 0.0001) / 20;
-            double regenAPPerTick = (maxAP * 0.0001) / 20;
-
-            cap.setAura(Math.min(cap.getAura() + regenAPPerTick, maxAP));
-            cap.setMagicule(Math.min(cap.getMagicule() + regenMPPerTick, maxMP));
+            double apGain = maxAP * 0.001D;
+            if (cap.getAura() + apGain < cap.getBaseAura()) {
+                cap.setAura(cap.getAura() + apGain);
+            } else {
+                cap.setAura(cap.getBaseAura());
+            }
         });
-
-        TensuraPlayerCapability.sync(player);
     }
 
     public void onDeath(ManasSkillInstance instance, LivingDeathEvent event) {
         if (!event.isCanceled()) {
             DamageSource source = event.getSource();
-            if (source != DamageSource.OUT_OF_WORLD) {
+            if (source != DamageSource.OUT_OF_WORLD &&
+                    !instance.onCoolDown()) {
                 if (source instanceof TensuraDamageSource) {
                     TensuraDamageSource damageSource = (TensuraDamageSource)source;
-                    if (damageSource.getIgnoreBarrier() >= 3.0F) {
+                    if (damageSource.getIgnoreBarrier() >= 3.0F)
                         return;
-                    }
                 }
-
                 LivingEntity entity = event.getEntity();
                 if (!entity.isAlive()) {
                     if (source.getEntity() != null) {
-                        if (source.getEntity() == entity) {
+                        if (source.getEntity() == entity)
                             return;
-                        }
-
-                        if (source.getEntity() == SkillHelper.getSubordinateOwner(entity)) {
+                        if (source.getEntity() == SkillHelper.getSubordinateOwner(entity))
                             return;
-                        }
                     }
-
-
-                        TensuraEffectsCapability.resetEverything(entity, false, false); //THIS RESETS EFFECTS NOTHING ELSE DON'T GET IDEAS
-                        TensuraEPCapability.getFrom(entity).ifPresent((cap) -> {
-                            if (cap.getEP() <= 0.0) {
-                                cap.setEP(entity, 100.0, false);
-                            } else if (cap.getCurrentEP() <= 0.0) {
-                                cap.setCurrentEP(entity, cap.getEP() * 0.5);
-                            }
-
-                            double SHP = entity.getAttributeValue((Attribute)TensuraAttributeRegistry.MAX_SPIRITUAL_HEALTH.get());
-                            if (cap.getSpiritualHealth() < SHP * 0.10) {
-                                cap.setSpiritualHealth(SHP * 0.10);
-                            }
-
-
-                        });
-                        TensuraEPCapability.sync(entity);
-
-                        event.setCanceled(true);
-                        if (!instance.onCoolDown()) {
-                            instance.setCoolDown(60);
-                        }
-
-                        entity.getLevel().playSound((Player)null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.TOTEM_USE, SoundSource.PLAYERS, 1.0F, 1.0F);
-                        TensuraParticleHelper.addServerParticlesAroundSelf(entity, ParticleTypes.TOTEM_OF_UNDYING, 1.0);
-                        TensuraParticleHelper.addServerParticlesAroundSelf(entity, ParticleTypes.TOTEM_OF_UNDYING, 2.0);
-                        TensuraParticleHelper.addServerParticlesAroundSelf(entity, ParticleTypes.FLASH, 1.0);
-                    }
+                    addMasteryPoint(instance, entity);
+                    entity.setHealth(Math.max(entity.getHealth(), entity.getMaxHealth() * 0.1F));
+                    entity.invulnerableTime = Math.max(60, entity.invulnerableTime);
+                    Predicate<MobEffect> predicate = effect -> (effect.getCategory() == MobEffectCategory.HARMFUL);
+                    SkillHelper.removePredicateEffect(entity, predicate);
+                    TensuraEffectsCapability.resetEverything(entity, false, false);
+                    event.setCanceled(true);
+                    if (!instance.onCoolDown())
+                        instance.setCoolDown(60);
+                    entity.level.playSound((Player)null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.PLAYER_LEVELUP, SoundSource.PLAYERS, 1.0F, 1.0F);
+                    TensuraParticleHelper.addServerParticlesAroundSelf((Entity)entity, (ParticleOptions)ParticleTypes.FIREWORK, 1.0D);
+                    TensuraParticleHelper.addServerParticlesAroundSelf((Entity)entity, (ParticleOptions) ParticleTypes.FIREWORK, 2.0D);
+                    TensuraParticleHelper.addServerParticlesAroundSelf((Entity)entity, (ParticleOptions)ParticleTypes.FIREWORK, 1.0D);
                 }
             }
+        }
     }
-
-
 }
-
