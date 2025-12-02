15beebb15338a53e3fa454a81facddc66f13e5f1
Jester <shirokodaki@gmail.com>
2025-12-02 14:21:53 +0200

    Revenant Line

--- src/main/java/com/github/mythos/mythos/config/MythosRacesConfig.java
+++ src/main/java/com/github/mythos/mythos/config/MythosRacesConfig.java
@@ -3,16 +3,16 @@ package com.github.mythos.mythos.config;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 public class MythosRacesConfig {
+
     public final ForgeConfigSpec.IntValue bossForValkyrie;
+    public final ForgeConfigSpec.IntValue essenceForApostle;
+
     public MythosRacesConfig(ForgeConfigSpec.Builder builder) {
         builder.push("evolutionEPRequirements");
         builder.pop();
         builder.pop();
         builder.push("evolutionRequirements");
         this.bossForValkyrie = builder.comment("The number of Boss kills needed to evolve into Valkyrie").defineInRange("bossForValkyrie", 4, 0, 10000);
+        this.essenceForApostle = builder.comment("The number of Cryptid Essence needed to evolve to Void Apostle").defineInRange("essenceForApostle", 5, 0, 10000);
     }
-
-
-
-
 }
