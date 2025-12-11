b3d5db154f6f2dba633a452cde444888ad2ec78b
electroreal1 <155461193+electroreal1@users.noreply.github.com>
2025-12-11 19:12:50 +0200

    adjusted Indra n Heaven's Wrath

--- src/main/java/com/github/mythos/mythos/util/ClientModEvents.java
+++ src/main/java/com/github/mythos/mythos/util/ClientModEvents.java
@@ -3,10 +3,8 @@ package com.github.mythos.mythos.util;
 import com.github.mythos.mythos.client.screen.OrunScreen;
 import com.github.mythos.mythos.registry.MythosEntityTypes;
 import com.github.mythos.mythos.registry.menu.MythosMenuTypes;
-import com.github.mythos.mythos.renderers.ThunderStormRenderer;
 import com.github.mythos.mythos.renderers.VajraBreathRenderer;
 import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -27,14 +25,14 @@ public class ClientModEvents {
     public static void onClientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
             MenuScreens.register(MythosMenuTypes.ORUN_MENU.get(), OrunScreen::new);
-            EntityRenderers.register(MythosEntityTypes.THUNDER_STORM.get(), ThunderStormRenderer::new);
+           // EntityRenderers.register(MythosEntityTypes.THUNDER_STORM.get(), ThunderStormRenderer::new);
         });
     }
 
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
 //        event.registerEntityRenderer(MythosEntityTypes.DRAGONFIRE.get(), DragonfireRenderer::new);
-        event.registerEntityRenderer(MythosEntityTypes.THUNDER_STORM.get(), ThunderStormRenderer::new);
+      //  event.registerEntityRenderer(MythosEntityTypes.THUNDER_STORM.get(), ThunderStormRenderer::new);
         event.registerEntityRenderer(MythosEntityTypes.VAJRA_BREATH.get(), VajraBreathRenderer::new);
     }
 }
