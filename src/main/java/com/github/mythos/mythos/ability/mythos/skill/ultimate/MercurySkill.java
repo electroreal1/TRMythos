3e0c3566ff99ab51764feff064bfe3f5724fb73d
electroreal1 <155461193+electroreal1@users.noreply.github.com>
2025-12-30 23:25:31 +0200

    mercury start will continue later

---
+++ src/main/java/com/github/mythos/mythos/ability/mythos/skill/ultimate/MercurySkill.java
@@ -0,0 +1,62 @@
+package com.github.mythos.mythos.ability.mythos.skill.ultimate;
+
+import com.github.manasmods.manascore.api.skills.ManasSkill;
+import com.github.manasmods.manascore.api.skills.ManasSkillInstance;
+import com.github.manasmods.tensura.ability.SkillUtils;
+import com.github.manasmods.tensura.ability.skill.Skill;
+import com.github.manasmods.tensura.capability.ep.TensuraEPCapability;
+import com.github.manasmods.tensura.registry.effects.TensuraMobEffects;
+import com.github.manasmods.tensura.registry.skill.UniqueSkills;
+import io.github.Memoires.trmysticism.registry.effects.MysticismMobEffects;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.github.mythos.mythos.config.MythosSkillsConfig.EnableUltimateSkillObtainment;
+
+public class MercurySkill extends Skill {
+    public MercurySkill(SkillType type) {
+        super(SkillType.ULTIMATE);
+    }
+
+    @Override
+    public double getObtainingEpCost() {
+        return 2000000;
+    }
+
+    public boolean meetEPRequirement(@NotNull Player player, double newEP) {
+        if (!EnableUltimateSkillObtainment()) return false;
+        double currentEP = TensuraEPCapability.getCurrentEP(player);
+        if (currentEP < getObtainingEpCost()) {
+            return false;
+        }
+        return SkillUtils.isSkillMastered(player, (ManasSkill) UniqueSkills.TRAVELER.get()) &&
+                SkillUtils.isSkillMastered(player, (ManasSkill) UniqueSkills.USURPER.get());
+    }
+
+    @Override
+    public boolean canBeToggled(ManasSkillInstance instance, LivingEntity entity) {
+        return true;
+    }
+
+    public @NotNull List<MobEffect> getImmuneEffects(ManasSkillInstance instance, @NotNull LivingEntity entity) {
+        List<MobEffect> list = new ArrayList<>();
+        if (instance.isToggled()) {
+            list.add((MobEffect) TensuraMobEffects.INFINITE_IMPRISONMENT.get());
+            list.add((MobEffect) TensuraMobEffects.MAGIC_INTERFERENCE.get());
+            list.add((MobEffect) TensuraMobEffects.SPATIAL_BLOCKADE.get());
+            list.add((MobEffect) TensuraMobEffects.ENERGY_BLOCKADE.get());
+            list.add((MobEffect) MysticismMobEffects.TIMESTOP.get());
+            list.add((MobEffect) MysticismMobEffects.TIMESTOP_CORE.get());
+        }
+        return list;
+    }
+
+
+
+
+}
