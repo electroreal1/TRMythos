32de3ab1bfb83391403341a800f190b32f384f6b
electroreal1 <155461193+electroreal1@users.noreply.github.com>
2026-02-14 19:01:15 +0200

    reincarnator now works perfectly

--- src/main/java/com/github/mythos/mythos/ability/mythos/skill/reincarnatorline/Reincarnator.java
+++ src/main/java/com/github/mythos/mythos/ability/mythos/skill/reincarnatorline/Reincarnator.java
@@ -1,8 +1,10 @@
 package com.github.mythos.mythos.ability.mythos.skill.reincarnatorline;
 
+import com.github.manasmods.manascore.api.skills.ManasSkillInstance;
 import com.github.manasmods.tensura.ability.skill.Skill;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import org.jetbrains.annotations.Nullable;
 
 public class Reincarnator extends Skill {
@@ -19,5 +21,10 @@ public class Reincarnator extends Skill {
     @Override
     public MutableComponent getColoredName() {
         return Component.literal("You've met with death time and time again, living numerous lives, yet your memories and sense of self remain immutable. Truly a unique existence.");
+    }
+
+    @Override
+    public void onDeath(ManasSkillInstance instance, LivingDeathEvent event) {
+        instance.addMasteryPoint(event.getEntity());
     }
 }
