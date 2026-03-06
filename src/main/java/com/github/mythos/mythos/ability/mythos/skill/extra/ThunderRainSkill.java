aec64fe84a809ae19cf442e2dced1ebb3ebcf1b6
electroreal1 <155461193+electroreal1@users.noreply.github.com>
2026-03-06 17:22:52 +0200

    thunder rain

--- src/main/java/com/github/mythos/mythos/ability/mythos/skill/extra/ThunderRainSkill.java
+++ src/main/java/com/github/mythos/mythos/ability/mythos/skill/extra/ThunderRainSkill.java
@@ -1,6 +1,5 @@
 package com.github.mythos.mythos.ability.mythos.skill.extra;
 
-import com.github.manasmods.manascore.api.skills.ManasSkill;
 import com.github.manasmods.manascore.api.skills.ManasSkillInstance;
 import com.github.manasmods.tensura.ability.SkillHelper;
 import com.github.manasmods.tensura.ability.SkillUtils;
@@ -11,11 +10,9 @@ import com.github.manasmods.tensura.entity.magic.projectile.LightningLanceProjec
 import com.github.manasmods.tensura.registry.skill.ExtraSkills;
 import com.github.mythos.mythos.registry.MythosMobEffects;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -24,9 +21,10 @@ import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 
 public class ThunderRainSkill extends Skill {
-    public ThunderRainSkill(SkillType type) {
+    public ThunderRainSkill() {
         super(SkillType.EXTRA);
     }
 
@@ -35,32 +33,36 @@ public class ThunderRainSkill extends Skill {
         return 2;
     }
 
-    public int nextMode(LivingEntity entity, TensuraSkillInstance instance, boolean reverse) {
+    public int nextMode(@NotNull LivingEntity entity, TensuraSkillInstance instance, boolean reverse) {
         return instance.getMode() == 1 ? 2 : 1;
     }
 
-    public Component getModeName(int mode) {
-        MutableComponent var10000;
-        switch (mode) {
-            case 1:
-                var10000 = Component.translatable("trmythos.skill.thunder_rain.rain");
-                break;
-            case 2:
-                var10000 = Component.translatable("trmythos.skill.thunder_rain.coat");
-                break;
-            default:
-                var10000 = Component.empty();
-        }
-        return var10000;
+    public @NotNull Component getModeName(int mode) {
+        return switch (mode) {
+            case 1 -> Component.translatable("trmythos.skill.thunder_rain.rain");
+            case 2 -> Component.translatable("trmythos.skill.thunder_rain.coat");
+            default -> Component.empty();
+        };
     }
 
+    @Override
+    public void onTick(ManasSkillInstance instance, LivingEntity entity) {
+        if (!entity.hasEffect(MythosMobEffects.LIGHTNING_COAT.get())) {
+            if (SkillHelper.outOfMagicule(entity, instance)) {
+                return;
+            }
 
-    public boolean meetEPRequirement(Player player, double newEP) {
+            entity.addEffect(new MobEffectInstance(MythosMobEffects.LIGHTNING_COAT.get(), 2400, 0, false, false, false));
+            entity.getLevel().playSound(null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.ILLUSIONER_MIRROR_MOVE, SoundSource.PLAYERS, 1.0F, 1.0F);
+        }
+    }
+
+    public boolean meetEPRequirement(@NotNull Player player, double newEP) {
         double currentEP = TensuraEPCapability.getCurrentEP(player);
         if (currentEP < getObtainingEpCost()) {
             return false;
         }
-        return SkillUtils.isSkillMastered(player, (ManasSkill) ExtraSkills.LIGHTNING_MANIPULATION.get());
+        return SkillUtils.isSkillMastered(player, ExtraSkills.LIGHTNING_MANIPULATION.get());
     }
 
     @Override
@@ -68,10 +70,10 @@ public class ThunderRainSkill extends Skill {
         return 1000;
     }
 
-    private void spawnThunderRain(ManasSkillInstance instance, LivingEntity entity, Vec3 pos, int arrowAmount, double distance) {
-        int arrowRot = 360 / arrowAmount;
+    private void spawnThunderRain(ManasSkillInstance instance, LivingEntity entity, Vec3 pos, double distance) {
+        int arrowRot = 360 / 10;
 
-        for(int i = 0; i < arrowAmount; ++i) {
+        for(int i = 0; i < 10; ++i) {
             Vec3 arrowPos = entity.getEyePosition().add((new Vec3(0.0, distance, 0.0))
                     .zRot(((float)(arrowRot * i) - (float)arrowRot / 2.0F) * 0.017453292F)
                     .xRot(-entity.getXRot() * 0.017453292F)
@@ -82,48 +84,48 @@ public class ThunderRainSkill extends Skill {
             arrow.shootFromRot(pos.subtract(arrowPos).normalize());
             arrow.setLife(50);
             arrow.setDamage(50);
-            arrow.setMpCost(this.magiculeCost(entity, instance) / (double)arrowAmount);
+            arrow.setMpCost(this.magiculeCost(entity, instance) / (double) 10);
             arrow.setSpiritAttack(false);
             arrow.setSkill(instance);
             entity.getLevel().addFreshEntity(arrow);
         }
 
     }
-    public void onPressed(ManasSkillInstance instance, LivingEntity entity) {
+    public void onPressed(ManasSkillInstance instance, @NotNull LivingEntity entity) {
         if (instance.getMode() == 1) {
             entity.swing(InteractionHand.MAIN_HAND, true);
             this.addMasteryPoint(instance, entity);
             Level level = entity.getLevel();
             int distance = instance.isMastered(entity) ? 30 : 20;
-            Entity target = SkillHelper.getTargetingEntity(entity, (double)distance, false, true);
+            Entity target = SkillHelper.getTargetingEntity(entity, distance, false, true);
             Vec3 pos;
             if (target != null) {
                 pos = target.getEyePosition();
             } else {
-                BlockHitResult result = SkillHelper.getPlayerPOVHitResult(entity.level, entity, ClipContext.Fluid.NONE, (double)distance);
+                BlockHitResult result = SkillHelper.getPlayerPOVHitResult(entity.level, entity, ClipContext.Fluid.NONE, distance);
                 pos = result.getLocation().add(0.0, 0.5, 0.0);
             }
 
             if (instance.isMastered(entity)) {
-                this.spawnThunderRain(instance, entity, pos, 10, 2.0);
-                this.spawnThunderRain(instance, entity, pos, 10, 4.0);
+                this.spawnThunderRain(instance, entity, pos, 2.0);
+                this.spawnThunderRain(instance, entity, pos, 4.0);
             } else {
-                this.spawnThunderRain(instance, entity, pos, 10, 3.0);
+                this.spawnThunderRain(instance, entity, pos, 3.0);
             }
 
-            level.playSound((Player)null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.ILLUSIONER_CAST_SPELL, SoundSource.PLAYERS, 1.0F, 1.0F);
+            level.playSound(null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.ILLUSIONER_CAST_SPELL, SoundSource.PLAYERS, 1.0F, 1.0F);
             instance.setCoolDown(15);
         } else if (instance.getMode() == 2) {
-            if (!entity.hasEffect((MobEffect) MythosMobEffects.LIGHTNING_COAT.get())) {
+            if (!entity.hasEffect(MythosMobEffects.LIGHTNING_COAT.get())) {
                 if (SkillHelper.outOfMagicule(entity, instance)) {
                     return;
                 }
 
-                entity.addEffect(new MobEffectInstance((MobEffect) MythosMobEffects.LIGHTNING_COAT.get(), 2400, 0, false, false, false));
-                entity.getLevel().playSound((Player)null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.ILLUSIONER_MIRROR_MOVE, SoundSource.PLAYERS, 1.0F, 1.0F);
+                entity.addEffect(new MobEffectInstance(MythosMobEffects.LIGHTNING_COAT.get(), 2400, 0, false, false, false));
+                entity.getLevel().playSound(null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.ILLUSIONER_MIRROR_MOVE, SoundSource.PLAYERS, 1.0F, 1.0F);
             } else {
-                entity.removeEffect((MobEffect)MythosMobEffects.LIGHTNING_COAT.get());
-                entity.getLevel().playSound((Player)null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.ILLUSIONER_MIRROR_MOVE, SoundSource.PLAYERS, 1.0F, 1.0F);
+                entity.removeEffect(MythosMobEffects.LIGHTNING_COAT.get());
+                entity.getLevel().playSound(null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.ILLUSIONER_MIRROR_MOVE, SoundSource.PLAYERS, 1.0F, 1.0F);
             }
         }
     }
